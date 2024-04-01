a83af4f4bf945c07f763650e80326c7334809127
Rodwell <matchon.rodwell@gmail.com>
2024-04-01 23:07:16 +0800

    modified act6

--- act6.py
+++ act6.py
@@ -2,3 +2,9 @@ daysOfWeek = ('Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 
 daysofWeek1 = list(daysOfWeek)
 daysofWeek1[0] = "Day"
 print(daysofWeek1[0])
+
+# Printing the type of daysOfWeek
+print("Type of daysOfWeek:", type(daysOfWeek))
+
+# Printing the type of daysofWeek1
+print("Type of daysofWeek1:", type(daysofWeek1))
